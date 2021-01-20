9db331cd82a65a18f9c080338062ac682ef66dcb
cree321 <42984414+cree321@users.noreply.github.com>
2021-01-19 21:45:13 -0800

    Update modelLoader.js

--- experiments/modelLoader.js
+++ experiments/modelLoader.js
@@ -2,8 +2,8 @@ const input = document.getElementById("file");
 const reader = new FileReader();
 
 input.addEventListener("change", (e) => {
-  reader.readAsText(input.files[0]);
+  reader.readAsDataURL(input.files[0]);
 })
 reader.onload = (e) => {
-  console.log(reader.result);
+  reader.result
 }
